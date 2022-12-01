d9aa139a63c795abe8281d39accc1e33e77a520c
XDTerminated <gupta.sayam2006@gmail.com>
2022-11-30 22:12:01 -0600

    created chess borad

--- core/src/com/mygdx/chess/Main.java
+++ core/src/com/mygdx/chess/Main.java
@@ -1,31 +1,50 @@
 package com.mygdx.chess;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
+	// Creating Variables
 	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		createChessBoard();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
+
+	public void createChessBoard() {
+		final Color WHITE = new Color(0x9c9c9c);
+		final Color BLACK = new Color(0x595959);
+		ScreenUtils.clear(252, 252, 252, 252);
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+				shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+				if ((j + i) % 2 == 1) {
+					shapeRenderer.setColor(WHITE);
+				} else {
+					shapeRenderer.setColor(BLACK);
+				}
+
+				shapeRenderer.rect(j * 220, i * 220, 220, 220);
+				shapeRenderer.end();
+			}
+		}
+	}
+
 }
