4c38942da2b39607a1472c157efa4f191fb76dce
XDTerminated <gupta.sayam2006@gmail.com>
2022-12-01 18:08:18 -0600

    Reset

--- core/src/com/mygdx/chess/Main.java
+++ core/src/com/mygdx/chess/Main.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-	// Test comment
 	// Creating Variables
 	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
